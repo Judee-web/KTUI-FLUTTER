437bf248c45514008101995b7cbd6acf6d11b3cb
ebi <ebidedabofa@gmail.comebidedabofa@gmail.com>
2023-03-14 22:18:00 -0700

    added Icon in homePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 
@@ -16,30 +18,58 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( title: Text('My Title', style: TextStyle(
-         fontWeight: FontWeight.bold,
-         color: Color.fromARGB(255, 253, 255, 254),
-         textA
-         )),
-      backgroundColor:Color.fromARGB(255, 173, 25, 25)
-       ),
       backgroundColor: Color.fromARGB(255, 37, 146, 236),
       body:SafeArea(
-        child: Column(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                  'Hello, Mate!',
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                     Text(
+                  'Hello, Jude!',
                   style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                   color: Color.fromARGB(255, 255, 251, 251)
-                ))
+                )),
+
+              Text(
+                '16 March, 2023',
+                style: TextStyle(color: Colors.blue[200]),
+              ),
+
+             
+                  ],
+                  
+                ),
+
+                Container(
+                //notification
+                decoration: BoxDecoration(
+                  color: Colors.blue[600],
+                   ),
+                  padding: EdgeInsets.all(12),
+                  child: Icon(
+                    Icons.notifications,
+                    color: Colors.white,
+
+                  ),
+               )
+               
+              
+
 
               ]
             )
           ],
 
-      ))
+      )))
+        
      
      
     );
